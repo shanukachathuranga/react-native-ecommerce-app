5d702acb568889250898dba83a029bfd8cfcd7b9
shanuka chathuranga <shanukachathuranga21@gmail.com>
2025-06-16 15:23:37 +0530

    refactoring

--- app/(onboarding)/index.tsx
+++ app/(onboarding)/index.tsx
@@ -4,6 +4,7 @@ import { onboardingSlides } from '@/lib/onboardingData'
 import OnboardingSlide from "../../components/onboarding/OnboardingSlide";
 import {router} from "expo-router";
 import {opacity} from "react-native-reanimated/lib/typescript/Colors";
+import Paging from "@/components/onboarding/Paging";
 
 const index = () => {
     const [currentIndex, setCurrentIndex] = useState(0)
@@ -53,25 +54,13 @@ const index = () => {
             viewabilityConfig={{ viewAreaCoveragePercentThreshold: 50 }}
 
         />
-        <View className=" w-screen h-12 mb-6 flex px-5 justify-between items-center flex-row">
-            <Pressable onPress={scrollPrev} className={`${currentIndex === 0 ? 'opacity-0' : 'opacity-100'} 
-            flex justify-center items-start w-auto h-full`} disabled={currentIndex===0}>
-                <Text className="font-montserrat text-2xl font-semibold text-splash-grey">
-                    Prev
-                </Text>
-            </Pressable>
-            <View className={`w-[200px] h-full flex-1 flex gap-5  items-center flex-row ${currentIndex === 2 ? 'justify-end pr-10': 'justify-center'}`}>
-                <View className={`h-[13px] rounded-full ${currentIndex === 0 ? 'w-[50px] bg-black':'w-[15px] bg-splash-grey'} `}></View>
-                <View className={`h-[13px] rounded-full ${currentIndex === 1 ? 'w-[50px] bg-black':'w-[15px] bg-splash-grey'} `}></View>
-                <View className={`h-[13px] rounded-full ${currentIndex === 2 ? 'w-[50px] bg-black':'w-[15px] bg-splash-grey'} l`}></View>
-            </View>
-            <Pressable onPress={currentIndex === 2 ? handleOnboardingExit : scrollNext} className="flex justify-center items-end w-auto h-full">
-                <Text className="font-montserrat text-2xl font-semibold text-red-500">
-                    {(currentIndex === 2) ? 'Get Started' : 'Next'}
-                </Text>
-            </Pressable>
 
-        </View>
+            <Paging currentIndex={currentIndex}
+                    handleOnboardingExit={handleOnboardingExit}
+                    scrollNext={scrollNext}
+                    scrollPrev={scrollPrev}
+            />
+
 
 
     </SafeAreaView>
---
+++ components/onboarding/Paging.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {View, Text, Pressable} from 'react-native';
+
+interface Props{
+    scrollPrev:() => void,
+    currentIndex:number,
+    handleOnboardingExit:() => void,
+    scrollNext:() => void,
+}
+
+const Paging = ({scrollPrev, currentIndex, handleOnboardingExit, scrollNext}:Props) => {
+  return (
+    <View  className=" w-screen h-12 mb-6 flex px-5 justify-between items-center flex-row">
+        <Pressable onPress={scrollPrev} className={`${currentIndex === 0 ? 'opacity-0' : 'opacity-100'} 
+            flex justify-center items-start w-auto h-full`} disabled={currentIndex===0}>
+            <Text className="font-montserrat text-2xl font-semibold text-splash-grey">
+                Prev
+            </Text>
+        </Pressable>
+        <View className={`w-[200px] h-full flex-1 flex gap-5  items-center flex-row ${currentIndex === 2 ? 'justify-end pr-10': 'justify-center'}`}>
+            <View className={`h-[13px] rounded-full ${currentIndex === 0 ? 'w-[50px] bg-black':'w-[15px] bg-splash-grey'} `}></View>
+            <View className={`h-[13px] rounded-full ${currentIndex === 1 ? 'w-[50px] bg-black':'w-[15px] bg-splash-grey'} `}></View>
+            <View className={`h-[13px] rounded-full ${currentIndex === 2 ? 'w-[50px] bg-black':'w-[15px] bg-splash-grey'} l`}></View>
+        </View>
+        <Pressable onPress={currentIndex === 2 ? handleOnboardingExit : scrollNext} className="flex justify-center items-end w-auto h-full">
+            <Text className="font-montserrat text-2xl font-semibold text-red-500">
+                {(currentIndex === 2) ? 'Get Started' : 'Next'}
+            </Text>
+        </Pressable>
+    </View>
+  );
+};
+
+export default Paging;
